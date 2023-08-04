4ea212e608bc3e1fbefcda4311b82639c64da00a
pravi-97 <praveesh.894@gmail.com>
2023-08-04 20:51:57 +0530

    connect audio to backend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { AudioRecorder } from 'react-audio-voice-recorder';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const addAudioElement = async (blob) => {
+
+        try {
+            let wavBlob = blob;
+            if (wavBlob) {
+                const formData = new FormData();
+                formData.append('audioData', wavBlob, 'audio.wav');
+                // formData.append('message', 'Are you getting it?');
+                // console.log('blob :', wavBlob);
+                console.log("date-time at start: ", Date.now().toString());
+                const response = await fetch('http://localhost:3001/transcribe', {
+                    method: 'POST',
+                    body: formData,
+                });
+                // const data = await response.json();
+                // console.log('Transcription Job Name:', data);
+                // Handle the response or do something with the jobName
+                const audioBlob = await response.blob();
+                const audioUrl = URL.createObjectURL(audioBlob);
+                console.log("date-time at end: ", Date.now().toString());
+
+                // Use the audioUrl to play the speech
+                const audioElement = new Audio(audioUrl);
+                audioElement.play();
+            }
+            // Handle the response or do something with the jobName
+        } catch (error) {
+            console.error('Error starting transcription:', error);
+        }
+    };
+
+
+    return (
+        <div>
+            <AudioRecorder
+                onRecordingComplete={addAudioElement}
+                audioTrackConstraints={{
+                    noiseSuppression: true,
+                    echoCancellation: true,
+                    // autoGainControl,
+                    // channelCount,
+                    // deviceId,
+                    // groupId,
+                    // sampleRate,
+                    // sampleSize,
+                }}
+                onNotAllowedOrFound={(err) => console.table(err)}
+                // downloadOnSavePress={true}
+                downloadFileExtension="wav"
+                mediaRecorderOptions={{
+                    audioBitsPerSecond: 128000,
+                }}
+                showVisualizer={true}
+            />
+            <br />
+        </div>
+    );
 }
-
-export default App;
